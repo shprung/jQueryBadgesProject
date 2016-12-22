089971584804061463590c59981d066c1ea2a8ab
Shai Shprung <shprung@SHAI-SHPRUNGs-iMac.local>
2016-12-22 09:45:47 -0800

    added my js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
-
-  // your code will go here
-
+ var url='https://www.codeschool.com/users/haro.json';
+ $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      var c=response.courses.completed,a;
+      for(i in c){
+        a="<div class='course'><h3 title='"+c[i].title+"'></h3><img src='"+
+          c[i].badge+"'><a href='"+c[i].url+"' target='_blank' class='btn btn-primary'>See Course</a></div>";
+        $('#badges').append(a);
+      }
+    }
+  });
 });
